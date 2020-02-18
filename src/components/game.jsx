74fa44bbd3819c26fcfd081597e33547f015d840
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-02-18 21:42:43 +0600

    Add Game component functionalities

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,21 +1,38 @@
 import React, {Component} from 'react';
 import Board from './board';
 
+const calculateWinner = () => {
+    return
+}
 class Game extends Component {
 
     state = {
-        history: [{squares: Array(9).fill(null)}]
+        history: [{squares: Array(9).fill(null)}],
+        stepNumber: 0,
+        xIsNext: true
     };
 
-    onClick = i => {
-        console.log(i);
+    handleClick = i => {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if(calculateWinner(squares) || squares[i]) {
+            return
+        }
+        squares[i] = this.state.xIsNext ? 'x' : 'o'
+        this.setState({
+            history: history.concat([{squares}]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     render() {
         return(
             <div>
                 <h1>Game Component</h1>
-                <Board onClick={this.onClick} squares={this.state.history[0]}/>
+                <Board onClick={this.handleClick} squares={this.state.history[0]}/>
             </div>  
         );
     }
