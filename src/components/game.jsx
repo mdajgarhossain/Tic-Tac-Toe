5cf76f7406ced08069764db7395eb7819e810c2b
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-02-18 22:08:11 +0600

    Implement game winning logic

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import Board from './board';
 
-const calculateWinner = () => {
-    return
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for(let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
 }
 class Game extends Component {
 
@@ -29,10 +47,15 @@ class Game extends Component {
     }
 
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        console.log(winner);
+
         return(
             <div>
                 <h1>Game Component</h1>
-                <Board onClick={this.handleClick} squares={this.state.history[0]}/>
+                <Board onClick={this.handleClick} squares={current.squares}/>
             </div>  
         );
     }
